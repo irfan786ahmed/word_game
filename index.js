87743685580c5770cd37c75922e59f655dcf3e64
Irfan Ahmed <irfanahmed4009@gmail.com>
2022-08-07 11:50:15 +0530

    two new scramble boxes

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const inputText = document.querySelector(".blank");
 const inputText1 = document.querySelector(".blank1");
+const inputText2 = document.querySelector(".blank2");
+const inputText3 = document.querySelector(".blank3");
 const clickButton = document.querySelector(".btn");
 const clickButton1 = document.querySelector(".btn1");
-const start = document.querySelector("h3");
+const clickButton2 = document.querySelector(".btn2");
+const clickButton3 = document.querySelector(".btn3");
+const start = document.querySelector("#ani");
 const start1 = document.querySelector("#bol");
-let play = false;
-let animals = ['tiger', "lion", "monkey", "deer", "goat", "elephant", "cow", "buffalo", "giraffe", "dino", "dolphin"]
-let animalNames = "";
-let randAnimalNames = "";
-let bollywood = ["sharukh", "amir", "salman", "katrina", "aftab", "ranveer", "shahid", "irfan", "tabbu", "kareena"]
-let bollywoodNames = "";
-let randBollyNames = "";
+const start2 = document.querySelector("#cric");
+const start3 = document.querySelector("#bran");
+let play , play1 , play2 , play3= false
+let animalNames , randAnimalNames , bollywoodNames ,randBollyNames , cricketNames , randCricketNames , brandNames,randBrandNames= "";
+let animals = ['tiger', "lion", "monkey", "deer", "goat", "elephant", "cow", "buffalo", "giraffe", "dino", "dolphin"];
+let bollywood = ["sharukh", "amir", "salman", "katrina", "aftab", "ranveer", "shahid", "irfan", "tabbu", "kareena"];
+let cricket = ["sachin","virat","afridi","babar","watson","gayle","steyn","dhoni","rohit","hasan ali","dilshan"];
+let brand = ["zara","mufti","wrangler","rayban","bata","louis vuitton","roadster","lee cooper","adidas","nike","puma"];
+
 
 
 const createNewWords = () => {
@@ -22,6 +28,16 @@ const bollyNewWords = () => {
     let randomNumber2 = Math.floor(Math.random() * bollywood.length);
     let tempBollyWords = bollywood[randomNumber2];
     return tempBollyWords;
+}
+const cricketNewWords = () => {
+    let randomNumber3 = Math.floor(Math.random() * cricket.length);
+    let tempCricketWords = cricket[randomNumber3];
+    return tempCricketWords;
+}
+const brandNewWords = () => {
+    let randomNumber4 = Math.floor(Math.random() * brand.length);
+    let tempBrandWords = brand[randomNumber4];
+    return tempBrandWords;
 }
 
 const scrambleWords = (arr) => {
@@ -44,6 +60,26 @@ const scrambleBollyWords = (arr1) => {
     }
     return arr1;
 }
+const scrambleCricketWords = (arr2) => {
+    for (let i = arr2.length - 1; i > 0; i--) {
+        let temp2 = arr2[i]
+        let j2 = Math.floor(Math.random() * (i + 1))
+
+        arr2[i] = arr2[j2];
+        arr2[j2] = temp2;
+    }
+    return arr2;
+}
+const scrambleBrandWords = (arr3) => {
+    for (let i = arr3.length - 1; i > 0; i--) {
+        let temp3 = arr3[i]
+        let j3 = Math.floor(Math.random() * (i + 1))
+
+        arr3[i] = arr3[j3];
+        arr3[j3] = temp3;
+    }
+    return arr3;
+}
 
 
 
@@ -51,7 +87,7 @@ clickButton.addEventListener("click", () => {
     if (!play) {
         play = true;
         clickButton.innerHTML = "guess";
-        inputText.style.display = "block";
+        inputText.style.cssText = "display:block;text-transform:lowercase";
         start.innerHTML = "Guess the Word";
         animalNames = createNewWords();
         randAnimalNames = scrambleWords(animalNames.split("")).join("");
@@ -76,10 +112,10 @@ clickButton.addEventListener("click", () => {
 })
 
  clickButton1.addEventListener("click", () => {
-    if (!play) {
-        play = true;
+    if (!play1) {
+        play1 = true;
         clickButton1.innerHTML = "guess";
-        inputText1.style.display = "block";
+        inputText1.style.cssText = "display:block;text-transform:lowercase";
         start1.innerHTML = "Guess the Word";
         bollywoodNames = bollyNewWords();
         randBollyNames = scrambleBollyWords(bollywoodNames.split("")).join("");
@@ -88,7 +124,7 @@ clickButton.addEventListener("click", () => {
     else {
         let writtenWord1 = inputText1.value;
         if (writtenWord1 === bollywoodNames) {
-            play = false;
+            play1 = false;
             start1.style.cssText = "font-size:25px;text-transform:uppercase"
             start1.innerHTML = `you are absolutely <br> right my friend`;
             clickButton1.style.backgroundColor = "rgb(189, 243, 81)"
@@ -98,7 +134,63 @@ clickButton.addEventListener("click", () => {
         }
         else {
             start1.style.cssText = "font-size:25px;text-transform:uppercase;text-align:center"
-            start1.innerHTML = `You are wrong.Please try again <br> ""${randBollyNames}""`;
+            start1.innerHTML = `You are wrong.<br>Please try again <br> ""${randBollyNames}""`;
+        }
+    }
+})
+
+
+ clickButton2.addEventListener("click", () => {
+    if (!play2) {
+        play2 = true;
+        clickButton2.innerHTML = "guess";
+        inputText2.style.cssText = "display:block;text-transform:lowercase";
+        start2.innerHTML = "Guess the Word";
+        cricketNames = cricketNewWords();
+        randCricketNames = scrambleCricketWords(cricketNames.split("")).join("");
+        start2.innerHTML = randCricketNames;
+    }
+    else {
+        let writtenWord2 = inputText2.value;
+        if (writtenWord2 === cricketNames) {
+            play2 = false;
+            start2.style.cssText = "font-size:25px;text-transform:uppercase"
+            start2.innerHTML = `you are absolutely <br> right my friend`;
+            clickButton2.style.backgroundColor = "rgb(189, 243, 81)"
+            clickButton2.innerHTML = "Guess Again";
+            clickButton2.style.color = "black"
+            inputText2.value = "";
+        }
+        else {
+            start2.style.cssText = "font-size:25px;text-transform:uppercase;text-align:center"
+            start2.innerHTML = `You are wrong.<br>Please try again <br> ""${randCricketNames}""`;
+        }
+    }
+})
+ clickButton3.addEventListener("click", () => {
+    if (!play3) {
+        play3 = true;
+        clickButton3.innerHTML = "guess";
+        inputText3.style.cssText = "display:block;text-transform:lowercase";
+        start3.innerHTML = "Guess the Word";
+        brandNames = brandNewWords();
+        randBrandNames = scrambleBrandWords(brandNames.split("")).join("");
+        start3.innerHTML = randBrandNames;
+    }
+    else {
+        let writtenWord3 = inputText3.value;
+        if (writtenWord3 === brandNames) {
+            play3 = false;
+            start3.style.cssText = "font-size:25px;text-transform:uppercase"
+            start3.innerHTML = `you are absolutely <br> right my friend`;
+            clickButton3.style.backgroundColor = "rgb(189, 243, 81)"
+            clickButton3.innerHTML = "Guess Again";
+            clickButton3.style.color = "black"
+            inputText3.value = "";
+        }
+        else {
+            start3.style.cssText = "font-size:25px;text-transform:uppercase;text-align:center"
+            start3.innerHTML = `You are wrong.<br>Please try again <br> ""${randBrandNames}""`;
         }
     }
 })
